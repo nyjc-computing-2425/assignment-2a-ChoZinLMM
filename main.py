5ebe7f8786d2bbeee872fac479408640f4987b7f
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-01 02:49:48 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 num = input('Enter a number (decimal or integer): ')
-# type your code here
+num1 = num.strip(' ')
+num2 = num1.replace('.', '')
+num3 = num2.lstrip('0')
+sf = len(num3)
 
-
-
-# do not change any code beyond this point
 print('The number', num, 'has', sf, 'significant figures.')
